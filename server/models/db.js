1086e137bf85ed0be305f0f099edd70909798068
VicLiang621 <vliang621@gmail.com>
2018-02-26 14:29:20 -0500

    try again

--- server/models/db.js
+++ server/models/db.js
@@ -7,23 +7,37 @@ var conString = process.env.DB_URL || "postgres://postgres:5432@localhost/" + us
 
 // setup initial table
 function createDefaultTable(){
-
-    knex.schema.withSchema('public').createTableIfNotExists('historical_data', function(table){
-        console.log("creating table");
-        table.increments('id');
-        table.string('location_name', 100);
-        table.integer('count');
-        table.timestamp('date');
-    })
-    .createTableIfNotExists('live_data', function(table){
-          table.increments('id');
-          table.string('location_name', 100);
-          table.integer('count');
-     }).then().catch(function(e) {
-        console.error(e);
-      })
+    return new Promise((resolve, reject) => {
+        knex.schema.withSchema('public').createTableIfNotExists('historical_data', function(table){
+            console.log("creating table");
+            table.increments('id');
+            table.string('location_name', 100);
+            table.integer('count');
+            table.timestamp('date');
+        })
+        .createTableIfNotExists('live_data', function(table){
+              table.increments('id');
+              table.string('location_name', 100);
+              table.integer('count');
+              resolve();
+         })
+         .catch(function(e) {
+           reject(e);
+         });
+    });
 }
 
+
+
+return new Promise(async (resolve, reject) => {
+    knex('historical_data').insert({'location_name':location_name, 'count': count, 'date':date})
+    .then()
+    .catch(function(e){
+        console.log(e);
+    })
+    var updatedCount = await incrementCount(location_name, count);
+    return resolve({'count':updatedCount[0]});
+});
 
 // get current "people count" of a location
 function getCountAtLocation(location_name) {
@@ -181,9 +195,12 @@ function incrementCount(location_name, count){
 }
 
 console.log("Going to create database");
-createDefaultTable();
-console.log("Created database relations");
-knex.destroy();
+async function main(){
+    await createDefaultTable();
+    console.log("Created database relations");
+    await knex.destroy();
+}
+main();
 module.exports.getCountAtLocation = getCountAtLocation;
 module.exports.getHistoricalForLocation = getHistoricalForLocation;
 module.exports.getHistoricalGraphData = getHistoricalGraphData;
